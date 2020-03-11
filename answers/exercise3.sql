c346714ab7cf50a0f267c9f3071623c55b9c0b19
cwilliams <coreywllms9@gmail.com>
2020-03-11 15:25:48 -0400

    part 1 2 3

---
+++ answers/exercise3.sql
@@ -0,0 +1,11 @@
+CREATE DATABASE myNewBD;
+USE myNewBD;
+CREATE TABLE Users
+(
+    User_ID   INT,
+    LastName  VARCHAR(255),
+    FirstName VARCHAR(255),
+    Address   VARCHAR(255),
+    City      VARCHAR(255)
+);
+SELECT * FROM Users;
